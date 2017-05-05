d94416da2795a376605ec78b86dfc7a3bf444ae2
Haoyu Gu <hgu42@uwo.ca>
2017-05-05 14:44:21 -0400

    Now placed the kernel from sector 2 to mem 0x1000

--- kernel/arch/x86/boot/boot.asm
+++ kernel/arch/x86/boot/boot.asm
@@ -3,7 +3,6 @@
     ;; Copyright (C) 2017 Haoyu Gu
     ;;
 section .data
-    KERNEL_SECT db 
 
 section .text
     global	_main
@@ -19,13 +18,22 @@ _main:
     mov     bx, 0007h
     mov     dl, 0
     int     10h
-    
-    jmp     GO_P_MODE
+    jmp     DEBUG_LOAD_SE_SECTOR
     ;; next, we will load the kernel to 0x10000
     LOADER_MSG db "Frog Loader Ver 0.1"
-    xor     ax, ax
-    
-    
+DEBUG_LOAD_SE_SECTOR:
+    mov     ax, 0
+    mov     es, ax
+    mov     bx, 0x1000          ;will load to 0x1000
+    mov     ah, 02
+    mov     al, 01
+    mov     ch, 00
+    mov     cl, 02              ;read sector 02
+    mov     dh, 00
+    int     13h
+    ;; check error
+    or      ah, ah
+    jnz     LOAD_ERROR
     ;; will go to 32bit
 GO_P_MODE:
     ;; we will switch to protect mode here
@@ -77,11 +85,25 @@ gdtr_addr:
 	dd gdt_table_start
 
 LOAD_TADPOLE:
-   
+    jmp     gdt_code_addr:0x1000
     ret
 LOAD_SECOND_STAGE_LOADER:
     call    LOAD_TADPOLE
     jmp     $
+LOAD_ERROR:
+    mov     ah, 02h
+    mov     bh, 00h
+    mov     dx, 0300h
+    int     10h
+    mov     ax, LOADER_ERROR_MSG
+    mov     bp, ax
+    mov     cx, 30
+    mov     ax, 1301h
+    mov     bx, 0007h
+    mov     dl, 0
+    int     10h
+    jmp     $
+    LOADER_ERROR_MSG db "Error! Failed to load kernel."
     times   510-($-$$) db 0
     dw      0xaa55
     
